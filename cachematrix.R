1667aebef466512644f81778e487a18a59d828fb
kisspetyus <kisspetyus@yahoo.com>
2015-03-22 12:20:52 +0100

    R Programming Assingment 2
    R Programming Assingment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -7,17 +7,17 @@
 ##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
@@ -28,14 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ##should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinv()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m          
-  ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m          
+    ## Return a matrix that is the inverse of 'x'
 }
